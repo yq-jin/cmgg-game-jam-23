9baa3cae3b0392392f97cb247ea5d88dd130ea9b
spooketti <jonliu168@gmail.com>
2023-01-09 22:38:52 -0800

    Added checkerboard setup

---
+++ Assets/Scripts/CodeBoard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeBoard : MonoBehaviour
+{
+    //1 = white
+    //2 = black
+    //0 = empty
+    private checkerboard = [
+        [0,1,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0],
+        [0,1,0,1,0,1,0,1],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [2,0,2,0,2,0,2,0],
+        [0,2,0,2,0,2,0,2],
+        [2,0,2,0,2,0,2,0],
+    ]
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
