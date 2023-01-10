6ce872b5654f54cc61e5a7aae07c30dd555adeee
spooketti <jonliu168@gmail.com>
2023-01-10 10:48:29 -0800

    Board Initilization needs optimization: deleted two sprites and replaced with two placeholders

--- Assets/Scripts/ChessBoard.cs
+++ Assets/Scripts/ChessBoard.cs
@@ -7,6 +7,10 @@ public class ChessBoard : MonoBehaviour
     //1 = white
     //2 = black
     //0 = empty
+    public GameObject redChecker;
+    public GameObject blackChecker;
+
+
     int[,] checkerboard = new int[8,8]
     {
         {0,1,0,1,0,1,0,1},
@@ -20,7 +24,28 @@ public class ChessBoard : MonoBehaviour
     };//this took a while to figure out because i don't use c#
     void Start()
     {
-        
+     for(int y=0;y<8;y++)
+     {
+        for(int x=0;x<8;x++)//temporary solutions let's optimzie this
+        {
+            //arrays are being a bad word so i'm doing this the cringe way
+            //please optimize this (and tell unity to stop using C# please this langauge is sol balding)
+            //why can't i put prefabs in an array??? (it is because we use object oriented hahahah please javascript save me)
+            switch(checkerboard[y,x])
+            {
+                case 1:
+                Instantiate(redChecker, new Vector2(x, y), Quaternion.identity);
+                break;
+                
+                case 2:
+                Instantiate(blackChecker, new Vector2(x, y), Quaternion.identity);
+                break;
+
+                default:
+                break;
+            }
+        }
+     }   
     }
 
     // Update is called once per frame
